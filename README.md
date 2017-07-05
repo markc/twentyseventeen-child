7ab9cd239d7e7133ef7f6237fe9812c81307ed39
Mark Constable <markc@renta.net>
2017-07-05 18:01:35 +1000

    Second attempt to get it right

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # TwentySeventeen Child
 
-A Wordpress v4.8+ TwentSeventeen child theme, to follow along and make your
+A Wordpress v4.8+ TwentySeventeen child theme, to follow along and make your
 own modifications... read this fine manual...
 
 https://codex.wordpress.org/Twenty_Seventeen
+
+Example site: https://markc.blog
--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -10,6 +10,5 @@
 
 ?>
 <div class="site-info">
-    Copyright &copy; 1995-<?php echo date("Y") ?> <a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>
-    and sponsored by <a href="<?php echo esc_url( __( 'https://renta.net/', 'rentanet' ) ); ?>">RentaNet</a>
+    Copyright &copy; <?php echo date("Y") ?> <?php bloginfo('name'); ?>
 </div><!-- .site-info -->
